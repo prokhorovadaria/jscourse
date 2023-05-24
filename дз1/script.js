e0df4160534a1195a34f4207a56e02662c55a905
Darya <72408903+prokhorovadaria@users.noreply.github.com>
2023-05-24 17:31:36 +0300

    Added lesson 1, 2

---
+++ дз1/script.js
@@ -0,0 +1,7 @@
+//let tempCelsius = prompt('Какая температура в Цельсиях?');
+//let tempFahrenheit = (9/5) * tempCelsius + 32;
+//console.log(tempFahrenheit);
+
+let name = prompt('Введите имя');
+let admin = name;
+console.log(admin);
